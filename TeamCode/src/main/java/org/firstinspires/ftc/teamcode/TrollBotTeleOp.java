f42f859ba7e1054872f98793b965109350363d2e
ColombiaRay <30420525+ColombiaRay@users.noreply.github.com>
2018-09-13 16:43:50 -0500

    2

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrollBotTeleOp.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class TrollBotTeleOp extends OpMode {
+
+
+    //Declare Variables
+
+    @Override
+    public void init() {
+        //Initialize Variables, Hardware map
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
